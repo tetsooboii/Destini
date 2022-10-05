018d700df60ba18228e27a522e41542074943667
Temirlan Bekkozha <bekkozha01@gmail.com>
2022-10-05 23:27:08 +0600

    final res

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+        
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userChoice)
+        
+        updateUI()
+    }
+    
+    
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+struct Story {
+    var title : String
+    var choice1 : String
+    let choice1Destination : Int
+    var choice2 : String
+    let choice2Destination : Int
+}
